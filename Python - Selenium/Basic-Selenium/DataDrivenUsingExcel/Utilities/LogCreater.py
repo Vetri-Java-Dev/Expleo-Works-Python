ffed511a483867c198a7f3faf167116f19c39d22
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-06-09 16:20:48 +0530

    pytest practices

--- Python - Selenium/Basic-Selenium/DataDrivenUsingExcel/Utilities/LogCreater.py
+++ Python - Selenium/Basic-Selenium/DataDrivenUsingExcel/Utilities/LogCreater.py
@@ -5,6 +5,7 @@ def logGenerator():
         filename="TestLogReport.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S %p"
+        datefmt="%Y-%m-%d %H:%M:%S %p",
+        force=True
     )
     return logging.getLogger()
